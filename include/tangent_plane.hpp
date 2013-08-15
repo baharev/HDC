09fc27b0e74035600200cd019e35c6415a2b6cc4
Ali Baharev <ali.baharev@gmail.com>
2013-08-15 23:17:07 +0200

    Added stub files for tangent plane

---
+++ include/tangent_plane.hpp
@@ -0,0 +1,23 @@
+//  Copyright (C) Ali Baharev, 2013
+//  All rights reserved.
+//
+//  Please refer to the LICENSE file in the top directory
+//
+#ifndef TANGENT_PLANE_HPP_
+#define TANGENT_PLANE_HPP_
+
+namespace hdc {
+
+class tangent_plane {
+
+public:
+
+    tangent_plane();
+
+private:
+
+};
+
+} // namespace hdc
+
+#endif
---
+++ src/tangent_plane.cpp
@@ -0,0 +1,16 @@
+//  Copyright (C) Ali Baharev, 2013
+//  All rights reserved.
+//
+//  Please refer to the LICENSE file in the top directory
+//
+
+#include "tangent_plane.hpp"
+
+namespace hdc {
+
+tangent_plane::tangent_plane() {
+
+}
+
+} // namespace hdc
+
